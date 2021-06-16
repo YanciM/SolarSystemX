8d9fccac379e514591da046e9d2a273f650c5d57
YanciM <yancimontiel@gmail.com>
2021-06-16 13:20:53 -0400

    planet 2 ready

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,5 +13,9 @@ public class Main {
         Feron.Heat="600000000000kj";
         Feron.Radius="823587325872385723895km/2";
         Feron.Temperature="100000000000000000000000000f";
+
+        Planet2 planet2=new Planet2();
+        planet2.Radius="45456456";
+        planet2.Size="354541";
     }
 }
---
+++ src/com/company/Planet2.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Planet2 {
+
+    String Size;
+    String Radius;
+}
